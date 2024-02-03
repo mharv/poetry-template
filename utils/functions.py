db7ef04f73496c2d190896deaa28be3a60e06680
mharv <mitch.k.harvey@gmail.com>
2024-02-03 19:02:54 +0000

    added a default arg example

--- utils/functions.py
+++ utils/functions.py
@@ -9,7 +9,7 @@ def get_n_numbers(n: int) -> List[int]:
     return [alter(x) for x in range(n)]
 
 
-def multiply_array(arr: List[int], scalar: int) -> List[int]:
+def multiply_array(arr: List[int], scalar: int = 1) -> List[int]:
     result = []
     for i in range(len(arr)):
         result.append(arr[i] * scalar)
